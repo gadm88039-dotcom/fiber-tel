d742b71fc49c7c61ca5db2f965ca2df211e6b81f
gadm88039-dotcom <gadm88039@gmail.com>
2026-05-02 00:25:38 +0200

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,15 @@
+ 
+     var swiper = new Swiper(".slide-swp", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable:true
+    },
+    autoplay:{
+        delay:2500,
+    },
+    loop:true
+  });
+
+
+
